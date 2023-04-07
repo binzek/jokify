10f44e3e245b53c7d611fc95654a82d5ece3107b
Abdul Wajid <abdulwajidnv@gmail.com>
2023-04-07 18:15:04 +0530

    NewJokeBtn component created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import JokeCard from "./components/JokeCard";
+import NewJokeBtn from "./components/NewJokeBtn";
 
 const App = () => {
   return (
     <div className="bg-gradient-radial from-yellow-400 to-orange-500 min-h-screen flex flex-col justify-center">
       <JokeCard />
+      <NewJokeBtn />
     </div>
   );
 };
