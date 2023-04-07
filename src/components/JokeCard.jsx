f7149f46d67e9faf5efda7e60ebfea906da67cfe
Abdul Wajid <abdulwajidnv@gmail.com>
2023-04-07 15:29:07 +0530

    Fix: Borders added to JokeCard

--- src/components/JokeCard.jsx
+++ src/components/JokeCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const JokeCard = () => {
   return (
-    <div className="bg-white/50 w-11/12 md:w-3/4 lg:w-3/5 xl:w-1/2 mx-auto px-5 py-2.5 rounded-2xl">
+    <div className="bg-white/50 w-11/12 md:w-3/4 lg:w-3/5 xl:w-1/2 mx-auto px-5 py-2.5 rounded-2xl border-2 border-b-8 border-r-8 border-black">
       <p className="text-center font-vt323 text-xl lg:text-2xl">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit iure
         est quisquam eius, cupiditate unde repellat voluptas adipisci, natus
