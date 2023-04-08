451dee104dc7b843610890f16e2d3b36a9a175b9
Abdul Wajid <abdulwajidnv@gmail.com>
2023-04-08 08:11:26 +0530

    Background color changing logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import JokeCard from "./components/JokeCard";
 import NewJokeBtn from "./components/NewJokeBtn";
 import BgSelector from "./components/BgSelector";
 
 const App = () => {
+  const [bgColor, setBgColor] = useState("");
+
   return (
-    <div className="bg-gradient-radial from-yellow-400 to-orange-500 min-h-screen flex flex-col justify-center">
-      <BgSelector />
+    <div
+      className={`bg-gradient-radial ${
+        bgColor ? bgColor : "from-yellow-400 to-orange-500"
+      } min-h-screen flex flex-col justify-center`}
+    >
+      <BgSelector setBgColor={setBgColor} />
       <JokeCard />
       <NewJokeBtn />
     </div>
--- src/components/BgSelector.jsx
+++ src/components/BgSelector.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-const BgSelector = () => {
+const BgSelector = ({ setBgColor }) => {
+  const colors = [
+    "from-yellow-400 to-orange-500",
+    "from-green-300 to-green-600",
+    "from-sky-200 to-blue-600",
+    "from-pink-300 to-rose-500",
+  ];
+
   return (
     <div className="flex justify-center mb-auto mt-4">
-      <div className="cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial from-yellow-400 to-orange-500 rounded-full border mx-2 border-black"></div>
-      <div className="cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial from-green-300 to-green-600 rounded-full border mx-2 border-black"></div>
-      <div className="cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial from-sky-300 to-blue-500 rounded-full border mx-2 border-black"></div>
-      <div className="cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial from-pink-300 to-rose-500 rounded-full border mx-2 border-black"></div>
+      <div
+        className={`cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial ${colors[0]} rounded-full border mx-2 border-black`}
+        onClick={() => setBgColor(colors[0])}
+      ></div>
+      <div
+        className={`cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial ${colors[1]} rounded-full border mx-2 border-black`}
+        onClick={() => setBgColor(colors[1])}
+      ></div>
+      <div
+        className={`cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial ${colors[2]} rounded-full border mx-2 border-black`}
+        onClick={() => setBgColor(colors[2])}
+      ></div>
+      <div
+        className={`cursor-pointer w-8 h-8 lg:w-10 lg:h-10 bg-gradient-radial ${colors[3]} rounded-full border mx-2 border-black`}
+        onClick={() => setBgColor(colors[3])}
+      ></div>
     </div>
   );
 };
